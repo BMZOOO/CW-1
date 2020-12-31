8b067e13be85db74238fd52a0d9ffd700aba03c9
BMZOOO <76647795+BMZOOO@users.noreply.github.com>
2020-12-31 04:49:33 +0300

    Initial Commit

---
+++ CW-1/CW_1App.swift
@@ -0,0 +1,17 @@
+//
+//  CW_1App.swift
+//  CW-1
+//
+//  Created by Batool  Jawaheri  on 12/31/20.
+//
+
+import SwiftUI
+
+@main
+struct CW_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
